118f7a50b9a8fd4700a9061b0ab243682f8b2236
Samioul51 <akmsamioulislam51@gmail.com>
2025-11-10 18:11:24 +0600

    Created Category API

--- index.js
+++ index.js
@@ -24,14 +24,36 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        const db=client.db('PawMart');
+        const categories=db.collection('categories');
+        const listings=db.collection('listings');
+        const orders=db.collection('orders');
+
+
+        // All categories
+
+        app.get('/categories',async (req,res)=>{
+            try{
+                const cat=await categories.find().toArray();
+                res.send({
+                    success:true,
+                    data:cat
+                });
+            }
+            catch(error){
+                res.status(500).send({
+                    success:false,
+                    message:error.message
+                });
+            }
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        
     }
 }
 
